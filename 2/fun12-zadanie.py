7f50c8d66b2ddeac1458292c7923bc1e07447192
rajkonkret <rajkonkret660@gmail.com>
2024-07-03 10:31:51 +0200

    [2.3] update fun12-zadanie.py

--- 2/fun12-zadanie.py
+++ 2/fun12-zadanie.py
@@ -3,4 +3,32 @@
 # z tych argumentów funkcja zbuduje słownik
 # należy pobieraż argumenty w pętli while
 # zastosować "kalwisz ucieczki"
-# ctrl / -? cmd /
+# ctrl / - cmd /
+
+def build_dict(first, name, age=None):
+    person = {'first': first, 'name': name}
+    if age:  # jesli True
+        person['age'] = age
+
+    return person
+
+
+print(bool(0))  # False
+print(build_dict("Radek", "Kowalski", 56))
+# {'first': 'Radek', 'name': 'Kowalski', 'age': 56}
+
+while True:
+    print("Podaj imię i nazwisko")
+    print("Wpisz q by wyjść")
+
+    f_name = input("imię")
+    if f_name == 'q':
+        break
+    l_name = input("nazwisko")
+    if l_name == 'q':
+        break
+    age = input("wiek")
+    if age == 'q':
+        break
+
+    print(build_dict(f_name, l_name, age))
