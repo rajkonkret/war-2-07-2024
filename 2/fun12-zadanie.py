baed1de4c074624f5424a1349f4fffcc2643a4da
rajkonkret <rajkonkret660@gmail.com>
2024-07-03 10:34:42 +0200

    [2.4] update fun12-zadanie.py

--- 2/fun12-zadanie.py
+++ 2/fun12-zadanie.py
@@ -1,7 +1,7 @@
 # napisac funkcję, która przyjmuje argumenty age, first, last
 # age powinien posiadać wartości domyślne
 # z tych argumentów funkcja zbuduje słownik
-# należy pobieraż argumenty w pętli while
+# należy pobierać argumenty w pętli while
 # zastosować "kalwisz ucieczki"
 # ctrl / - cmd /
 
