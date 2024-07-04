69a8c40228ff630cef6434efcf21b932b74ed880
rajkonkret <rajkonkret660@gmail.com>
2024-07-04 14:36:43 +0200

    [3.8] update kl10

--- 3/kl10.py
+++ 3/kl10.py
@@ -12,6 +12,14 @@ class Counter(ABC):
     def drukuj(self):
         pass
 
+    @classmethod  # metoda zwraca obiekt klasy Counter
+    def from_counter(cls, counter):
+        return cls(counter.values)
+
+    @staticmethod  # metoda statyczna, nie wymaga obiektu tej klasy
+    def from_string():
+        print("Metoda statyczna")
+
 
 class BoundedCounter(Counter):
     MAX = 100
@@ -23,6 +31,12 @@ class BoundedCounter(Counter):
 
     def drukuj(self):
         print("Drukuje", self.values)
+
+
+class NewCounter(Counter):
+    """
+    Klasa dziedziczy po Counter
+    """
 
 
 # TypeError: Can't instantiate abstract class Counter without an implementation for abstract method 'drukuj'
@@ -43,3 +57,23 @@ bc2.increment()
 bc2.drukuj()
 # Drukuje 11
 # Drukuje 33
+
+
+# obiekty są polimorficzne bo mają wspólne elementy
+lista = [bc, bc2]
+for i in lista:
+    i.drukuj()
+# TypeError: Can't instantiate abstract class NewCounter without an implementation for abstract method 'drukuj'
+# NewCounter nie ma metody drukuj nadpisanej
+# nc = NewCounter()
+
+a = BoundedCounter.from_counter(bc)
+print(a)
+print(type(a))
+# <__main__.BoundedCounter object at 0x000001BE841AE930>
+# <class '__main__.BoundedCounter'>
+a.drukuj()  # Drukuje 11
+
+# uzycie metody statycznej, nie wymaga obiektu
+BoundedCounter.from_string()
+# Metoda statyczna
