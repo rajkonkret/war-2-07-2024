f1cf65c991e8fc066fe93924060a919470f51c4b
rajkonkret <rajkonkret660@gmail.com>
2024-07-05 10:55:13 +0200

    [4.7] add baza2 - cm.py.py

--- 4/baza1.py
+++ 4/baza1.py
@@ -1,12 +1,12 @@
 import sqlite3
 
 try:
-    sql_conetion = sqlite3.connect('sqlite_db.db')
-    cursor = sql_conetion.cursor()
+    sql_conection = sqlite3.connect('sqlite_db.db')
+    cursor = sql_conection.cursor()
     print("Baza danych zostałą podłączona")
 except sqlite3.Error as e:
     print("Bład podczas podłączania bazy danych", e)
 finally:
-    if sql_conetion:
-        sql_conetion.close()
+    if sql_conection:
+        sql_conection.close()
         print("Baza danych zostałą zamknięta")
